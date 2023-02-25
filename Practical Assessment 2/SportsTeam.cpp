43b683ac75511e406f37911c444fb6a524f392b3
WillowSaysWhat <126318401+WillowSaysWhat@users.noreply.github.com>
2023-02-25 09:45:48 +0000

    Assessment 2 Initial
    pushing the initial file

---
+++ Practical Assessment 2/SportsTeam.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+class Match
+{
+
+};
+class Team :Match
+{
+
+};
+
+class Player : Team
+{
+
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
